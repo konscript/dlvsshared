9cbd22f158ce65b9fe9b5bb2759386514ea9ffaa
laander <la@laander.com>
2012-12-04 01:22:55 +0100

    new vaccination group header/hover text and restyled query ui to better fit

--- wp-content/themes/whitelight/includes/dlvs/translations.php
+++ wp-content/themes/whitelight/includes/dlvs/translations.php
@@ -35,22 +35,22 @@ function dlvs_translate($key) {
 		"Updated Malaria Map" => "Opdateret Malaria kort",			// Country
 		"Latest Disease Surveillance" => "Sidste sygdomsovervågning", // Country
 		"Call to book" => "Ring for at booke", 		// clinic,
-		"All travelers" => "Alle rejsende",
-		"+2 weeks" => "+2 uger",
-		"+3 months" => "+3 måneder",
-		"+6 months" => "+6 måneder",
-		"All travelers description" => "Omfatter forretnings- eller kongresrejse af nogle dages varighed til hovedstad eller anden
+		"All travelers" => "Gruppe 1",
+		"+2 weeks" => "Gruppe 2",
+		"+3 months" => "Gruppe 3",
+		"+6 months" => "Gruppe 4",
+		"All travelers description" => "Alle rejsende. Omfatter forretnings- eller kongresrejse af nogle dages varighed til hovedstad eller anden
 storby. Er rejsen af særlig art med mulighed for intens smitteudsættelse, kan Gr. 1 suppleres
 med vacciner fra Gr. 2, 3 eller 4.",
-		"+2 weeks description" => "Omfatter arrangeret turistrejse af op til fire ugers varighed med dagsudflugter. Er rejsen af
+		"+2 weeks description" => "1-4 ugers rejsende. Omfatter arrangeret turistrejse af op til fire ugers varighed med dagsudflugter. Er rejsen af
 særlig art med mulighed for intens smitteudsættelse, kan Gr. 2 suppleres med vacciner fra Gr.
 3 eller 4. Det gælder fx indvandrere på familiebesøg (uanset rejsens varighed), ved seksuel
 kontakt med lokale, ved udtalt dårlig hygiejne (tyfus), ved tæt lokal personkontakt (hep. B),
 ved ophold med insekteksposition som fx trekkingtur (japansk encephalitis).",
-		"+3 months description" => "Omfatter individuel rejse af nogle måneders varighed, fx rygsækrejse. Er rejsen af særlig art
+		"+3 months description" => "1-6 måneders rejsende. Omfatter individuel rejse af nogle måneders varighed, fx rygsækrejse. Er rejsen af særlig art
 med mulighed for intens smitteudsættelse, kan Gr. 3 suppleres med vacciner fra Gr. 4. Det
 gælder fx indvandrere på familiebesøg (uanset rejsens varighed).",
-		"+6 months description" => "Omfatter langvarig individuel rejse i halve år, indvandrere på familiebesøg (uanset rejsens
+		"+6 months description" => "6 måneders rejsende. Omfatter langvarig individuel rejse i halve år, indvandrere på familiebesøg (uanset rejsens
 varighed), udstationering eller tilsvarende hyppigt gentagne besøg.",
 		"See a map of malaria risk for this country" => "Kort over aktuel malariarisiko i landet",
 		"Information on outbreaks from NaTHNaC" => "Information om sygdomsudbrud i landet",
