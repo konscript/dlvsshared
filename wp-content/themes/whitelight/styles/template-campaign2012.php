062e13012dc5a276c4b67d755006faeea2056184
Søren Louv <sorenlouv@gmail.com>
2012-12-07 15:05:13 +0100

    Added links to campaign

--- wp-content/themes/whitelight/styles/template-campaign2012.php
+++ wp-content/themes/whitelight/styles/template-campaign2012.php
@@ -38,11 +38,11 @@ h1#top-header {
                 <img src="<?php bloginfo('template_directory'); ?>/images/feriebilleder.jpg" alt="" />
             </div>
             <div class="landingpage">
-                <h1>Online booking</h1>
+                <h1><a href="/booking/">Online booking</a></h1>
                  Her kan du bestille tid direkte i et af vores 25 vaccinationscentre. Vi har åbent dag, aften og weekends så du ikke behøver tage fri fra job eller hive børnene ud af skolen.
-                <h2>Hvor skal I hen?</h2>
+                <h2><a href="/vaccinationsanbefaling">Hvor skal I hen?</a></h2>
                  Her kan du se hvilke vaccinationer der anbefales til dit rejsemål.
-                <h2>1/2 pris</h2>
+                <h2><a href="/features/fa-50-tilskud/">1/2 pris</a></h2>
                  Hvis du er medlem af Sygeforsikringen Danmark får du 50% i tilskud uanset hvilken gruppe du er medlem af.
                 <h2>Alle kan blive vaccineret hos os</h2>
                  Vi har gjort rejsevaccination enkelt og moderne. Du behøver ingen henvisning fra egen læge elller lignende. Du bestiller blot en tid og så klarer vi resten. <img style="float: right;" src="<?php bloginfo('template_directory'); ?>/images/dlvs_logo_long.png" alt="" />
