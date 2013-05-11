8b8983cc955b4717771a7573353fabc682bfe884
laander <la@laander.com>
2013-05-11 23:57:06 +0200

    home banner box pops out and misc

--- wp-content/themes/whitelight/header.php
+++ wp-content/themes/whitelight/header.php
@@ -118,7 +118,7 @@
 
 		    <ul id="side-nav" class="side-nav nav fl">
 		    	<li class="">
-		    		<span><?php echo dlvs_translate("We're here to help, call us 8-17 all days"); ?></span>
+		    		<span><?php echo dlvs_translate("Need help? Call us on weekdays from 9am - 5pm."); ?></span>
 		    	</li>
 		    </ul>
 		</div>
